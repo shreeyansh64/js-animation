956031e033a9d7d569242f2447f3b0c0cd84a4a6
shreeyansh64 <shreeyansh64@gmail.com>
2025-09-19 18:27:57 +0530

    testing some canvas animations

---
+++ script.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById('canvas_1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.fillStyle = 'green';
+
+
+class Particle {
+    constructor(effect){
+        this.effect = effect;
+        this.x = Math.random() * this.effect.width;
+        this.y = Math.random() * this.effect.height;
+        this.radius = 15;
+    }
+    draw(context){
+        context.beginPath();
+        context.arc(this.x,this.y,this.radius,0,Math.PI * 2);
+        context.fill();
+    }
+}
+
+class Effect {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.particles = [];
+        this.numberOfParticles =20;
+        this.createParticles();
+    }
+    createParticles(){
+        for (let i = 0; i < this.numberOfParticles; i++) {
+            this.particles.push(new Particle(this));
+        }
+    }
+    handle(context){
+        this.particles.forEach(e=>e.draw(context));
+    }
+}
+
+const eff = new Effect(canvas);
+eff.handle(ctx)
+
+
+function animation() {
+
+}
