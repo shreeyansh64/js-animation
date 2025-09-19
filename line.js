966054c78079c86b5df9bca7e6c039ad80f42408
shreeyansh64 <shreeyansh64@gmail.com>
2025-09-19 21:18:27 +0530

    changed circle radius

--- line.js
+++ line.js
@@ -37,7 +37,7 @@ window.addEventListener('mouseup', () => {
 class Particle {
     constructor(effect) {
         this.effect = effect;
-        this.radius = Math.random() * 20;
+        this.radius = Math.random() * 5;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
         this.vx = Math.random() * 2 - 1;
