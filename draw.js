44c8372097aa71e913227095dc67fc85d410ddeb
BALLU <singhaditya5382@gmail.com>
2025-09-20 11:56:27 +0530

    updated some more lines

--- draw.js
+++ draw.js
@@ -6,25 +6,32 @@ const ctx = canvas.getContext('2d');
 class Line {
     constructor(canvas) {
         this.canvas = canvas;
-        this.startx = Math.random() * this.canvas.width;
-        this.starty = Math.random() * this.canvas.height;
-        this.endx = Math.random() * this.canvas.width;
-        this.endy = Math.random() * this.canvas.height;
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.past = [{x:this.x,y:this.y}];
         this.hue = Math.floor(Math.random() * 360);
     }
     draw(context){
         context.strokeStyle = 'hsl('+this.hue+',100%, 50%)';
         context.lineWidth = Math.floor(Math.random() * 15 + 1);
         context.beginPath();
-        context.moveTo(this.startx,this.starty);
-        context.lineTo(this.endx,this.endy);
+        context.moveTo(this.past[0].x , this.past[0].y);
+        for (let i = 0; i < 10; i++) {
+            this.x = Math.random() * this.canvas.width;
+            this.y = Math.random() * this.canvas.height;
+            this.past.push({x:this.x,y:this.y})
+        }
+        for (let i = 0; i < this.past.length; i++) {
+            context.lineTo(this.past[i].x,this.past[i].y);
+        }
+        
         context.stroke();
         
     }
 }
 
 let lineArr = [];
-const totalLines = 50;
+const totalLines = 10;
 
 for (let i = 0; i < totalLines; i++) {
     lineArr.push(new Line(canvas));
