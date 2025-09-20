7a4cd972b48bb4e76a4b0df7b1f47e27a1a7c11b
BALLU <singhaditya5382@gmail.com>
2025-09-20 12:11:18 +0530

    added update function

--- draw.js
+++ draw.js
@@ -8,32 +8,46 @@ class Line {
         this.canvas = canvas;
         this.x = Math.random() * this.canvas.width;
         this.y = Math.random() * this.canvas.height;
-        this.past = [{x:this.x,y:this.y}];
+        this.past = [{ x: this.x, y: this.y }];
         this.hue = Math.floor(Math.random() * 360);
+        this.maxLength = 5;
     }
-    draw(context){
-        context.strokeStyle = 'hsl('+this.hue+',100%, 50%)';
+    draw(context) {
+        context.strokeStyle = 'hsl(' + this.hue + ',100%, 50%)';
         context.lineWidth = Math.floor(Math.random() * 15 + 1);
         context.beginPath();
-        context.moveTo(this.past[0].x , this.past[0].y);
-        for (let i = 0; i < 10; i++) {
-            this.x = Math.random() * this.canvas.width;
-            this.y = Math.random() * this.canvas.height;
-            this.past.push({x:this.x,y:this.y})
-        }
+        context.moveTo(this.past[0].x, this.past[0].y);
         for (let i = 0; i < this.past.length; i++) {
-            context.lineTo(this.past[i].x,this.past[i].y);
+            context.lineTo(this.past[i].x, this.past[i].y);
         }
-        
+
         context.stroke();
-        
+
+    }
+    update() {
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.past.push({ x: this.x, y: this.y });
+        if (this.past.length > this.maxLength) {
+            this.past.shift()
+        }
     }
 }
 
 let lineArr = [];
-const totalLines = 10;
+const totalLines = 1;
 
 for (let i = 0; i < totalLines; i++) {
     lineArr.push(new Line(canvas));
 }
-lineArr.map(e=>{e.draw(ctx)});
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lineArr.map(e=>{
+        e.draw(ctx);
+        e.update();
+    });
+    requestAnimationFrame(animate);
+}
+animate();
